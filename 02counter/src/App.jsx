13519360d955364f8d32101ed0cc5e9af1d90e45
git-Sahitya <sahitya5289@gmail.com>
2024-08-23 10:08:03 +0530

    hookes

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+   let [card , setCard]= useState(7)
+
+const addValue = () => {
+ setCard(card + 1)
+}
+
+const removeValue = () => {
+ setCard(card - 1)
+}
+
+
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value : {card}</h2>
+
+    <button
+      onClick={addValue}
+    >Add Value-------{card}</button>
+    <br/>
+    <button
+    onClick={removeValue}>Remove Value------{card}</button>
+    <p>footer : {card}</p>
+    </>
+  )
+}
+
+export default App
